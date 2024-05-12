55b23d0559e6b1c978c88e7a8ee0a3dab00802d7
Ricardo Teixeira <rmi.teixeira@campus.fct.unl.pt>
2024-05-12 12:10:39 +0100

    Categorical CrossEntrophy Function calc

---
+++ p7-loss.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import math
+
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+
+
+"""Raw Python"""
+loss = -(math.log(softmax_output[0])*target_output[0] +
+         math.log(softmax_output[1])*target_output[1] +
+         math.log(softmax_output[2])*target_output[2]
+        )
+
+print(loss)
+print(-math.log(softmax_output[0]))
+
+
+
+
+
+
+
+"""Numpy version"""
+
