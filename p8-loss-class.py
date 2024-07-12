fixed loss, implemented backprop, optimizer, lr decay and momentum

p8-loss-class.py
@@ -33,7 +33,7 @@ class Loss_CategoricalCrossEntropy(Loss):
         samples = len(y_pred)
         y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
         if len(y_true.shape) == 1:
-            confidences = y_pred_clipped[:, y_true]
+            confidences = y_pred_clipped[range(samples), y_true]
         elif len(y_true.shape) == 2:
             confidences = np.sum(y_pred_clipped*y_true, axis=1)
         